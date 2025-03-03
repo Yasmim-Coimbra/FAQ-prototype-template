79805d70d0ccdc6b30d602ea56d025e3073c4d0b
Yasmim <ymdsc.coimbra85@gmail.com>
2025-03-03 15:24:01 -0300

    separating responsibilities

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,24 @@ const redArrow = document.querySelectorAll(".red");
 
 questions.forEach((question, indexPergunta) => {
     question.addEventListener("click", () => {
-        answers[indexPergunta].classList.toggle("active");
-        grayArrow[indexPergunta].classList.toggle("on");
-        redArrow[indexPergunta].classList.toggle("on");
 
-        answers.forEach((answer, indexResposta) => {
-            if (indexResposta !== indexPergunta) {
-                answer.classList.remove("active");
-                grayArrow[indexResposta].classList.add("on");
-                redArrow[indexResposta].classList.remove("on");
-            }
-        })
+        turnOnOff(indexPergunta);
+        closeOtherAnswers();
+
+        function closeOtherAnswers() {
+            answers.forEach((answer, indexResposta) => {
+                if (indexResposta !== indexPergunta) {
+                    answer.classList.remove("active");
+                    grayArrow[indexResposta].classList.add("on");
+                    redArrow[indexResposta].classList.remove("on");
+                }
+            });
+        }
     })
 })
+
+function turnOnOff(indexPergunta) {
+    answers[indexPergunta].classList.toggle("active");
+    grayArrow[indexPergunta].classList.toggle("on");
+    redArrow[indexPergunta].classList.toggle("on");
+}
